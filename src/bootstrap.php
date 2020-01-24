d0ee2f87fc6c9160f6dc527f672d36ec20536588
zarlo <zarlo5899@gmail.com>
2020-01-24 14:08:10 +1100

    version moved to hash

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -31,8 +31,9 @@ function version() {
     exec('git describe --always',$version_mini_hash);
     exec('git rev-list HEAD | wc -l',$version_number);
     exec('git log -1',$line);
-    $version['short'] = "v1.".trim($version_number[0]).".".$version_mini_hash[0];
-    $version['full'] = "v1.".trim($version_number[0]).".$version_mini_hash[0] (".str_replace('commit ','',$line[0]).")";
+    $version['short'] = trim($version_number[0]).".".$version_mini_hash[0];
+    $version['full'] =  trim($version_number[0]).".$version_mini_hash[0] (".str_replace('commit ','',$line[0]).")";
+    $version['hash'] =  str_replace('commit ','',$line[0]);
     return $version;
   }
 
